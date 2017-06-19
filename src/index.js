89ac08952492c65064febd52ce6046460cbf4f78
JGBrown <j.grant.brown@gmail.com>
2017-06-19 13:52:13 -0400

    add clickable boxes with 0 or x need logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
+// sets turn to either odd or even to decide whos turn and X or O
+var turn = 0
 
 $('td').click(function() {
-   if ($(this).text() == ""){
-    $(this).text('X');
+ if ($(this).text()=== "") {
+  if (turn % 2 === 0){
+      $(this).text('X');
+    }else {
+        $(this).text('O');
   }
+turn++;}
 })
+
+
 
 
 $("#reset").on("click", function() {
