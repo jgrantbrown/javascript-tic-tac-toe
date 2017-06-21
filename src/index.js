d7676d9ad4e879a2e40526f39d2f55e5d740c959
JGBrown <j.grant.brown@gmail.com>
2017-06-21 12:24:01 -0400

    cleaned up a bit

--- src/index.js
+++ src/index.js
@@ -1,52 +1,75 @@
+var turn = 0
+
+
+
 $(document).ready(function(){
 
 // sets turn to either odd or even to decide whos turn and X or O
-var turn = 0
+    $('td').click(function() {
 
-$('td').click(function() {
-   if ($(this).text()=== "") {
-    if (turn % 2 === 0){
-        $(this).text('X');
-        $("#urturn").text("It is O's Turn");
-         checkWinner('X')
-    }else {
-          $("#urturn").text("It is X's Turn");
-          $(this).text('O');
-          checkWinner('O')
-          };
-        }
-turn++;
-  })
+     if ($(this).text()=== "") {
+        if (turn % 2 === 0){
+            $(this).text('X');
+            $("#urturn").text("It is O's Turn");
+              checkWinner('X')
+        }else {
+              $("#urturn").text("It is X's Turn");
+              $(this).text('O');
+              checkWinner('O')
+              }
+      }
+    // checkTie()
+    console.log(turn)
+  });
+
+})
+
 
 
 $("#reset").on("click", function() {
     $("#board td").empty()
   });
 
-});
+// });
 
 
 function checkWinner(player){
 //  1|2|3
 //  4|5|6
 //  7|8|9
-  let row1win = [$('#1')[0].innerHTML,$('#2')[0].innerHTML,$('#3')[0].innerHTML]
-  let row2win = [$('#4')[0].innerHTML,$('#5')[0].innerHTML,$('#6')[0].innerHTML]
-  let row3win = [$('#7')[0].innerHTML,$('#8')[0].innerHTML,$('#9')[0].innerHTML]
-  let col1win = [$('#1')[0].innerHTML,$('#4')[0].innerHTML,$('#7')[0].innerHTML]
-  let col2win = [$('#2')[0].innerHTML,$('#5')[0].innerHTML,$('#8')[0].innerHTML]
-  let col3win = [$('#3')[0].innerHTML,$('#6')[0].innerHTML,$('#9')[0].innerHTML]
-  let diag1win = [$('#1')[0].innerHTML,$('#5')[0].innerHTML,$('#9')[0].innerHTML]
-  let diag2win = [$('#3')[0].innerHTML,$('#5')[0].innerHTML,$('#7')[0].innerHTML]
+
+if (player == "X"){
+  plyr = 1
+  nextup= "O"
+}else if(player == "O"){
+  plyr = 0
+  nextup ="X"
+}
+  let row1 = [$('#1')[0].innerHTML,$('#2')[0].innerHTML,$('#3')[0].innerHTML]
+  let row2 = [$('#4')[0].innerHTML,$('#5')[0].innerHTML,$('#6')[0].innerHTML]
+  let row3 = [$('#7')[0].innerHTML,$('#8')[0].innerHTML,$('#9')[0].innerHTML]
+  let col1 = [$('#1')[0].innerHTML,$('#4')[0].innerHTML,$('#7')[0].innerHTML]
+  let col2 = [$('#2')[0].innerHTML,$('#5')[0].innerHTML,$('#8')[0].innerHTML]
+  let col3 = [$('#3')[0].innerHTML,$('#6')[0].innerHTML,$('#9')[0].innerHTML]
+  let diag1 = [$('#1')[0].innerHTML,$('#5')[0].innerHTML,$('#9')[0].innerHTML]
+  let diag2 = [$('#3')[0].innerHTML,$('#5')[0].innerHTML,$('#7')[0].innerHTML]
 
 
-  let winningcombos =[row1win,row2win,row3win,col1win,col2win,col3win,diag1win,diag2win]
+  let combos =[row1,row2,row3,col1,col2,col3,diag1,diag2]
   let winner = [player,player,player]
-  combostring= JSON.stringify(winningcombos)
+  combostring= JSON.stringify(combos)
   winnerstring= JSON.stringify(winner)
 
       if (combostring.includes(winnerstring)){
-          setTimeout(function(){alert('Game over! '+player+' is the winner!')},500);
-          setTimeout(function(){ $("#board td").empty() }, 1000)
+          $("#urturn").text(player+" Wins!")
+          setTimeout(function(){ $("#board td").empty() }, 1000);
+          setTimeout(function(){$("#urturn").text(nextup+"'s Turn")},2000)
+          turn = plyr
+        }else{
+          turn++;
         }
+
   }
+
+  // function checkTie(){
+  // }
